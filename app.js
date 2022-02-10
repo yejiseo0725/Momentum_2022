1f07f79fac0cdf8cf3c3ace8dd1c5507f9fe86d6
yeji seo <yejiseo0725@gmail.com>
2022-02-10 21:26:55 +0900

    day 4

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-// document: 브라우저에 이미 존재하는 object
-// 내가 접근할 수 있는 HTML 을 가리키는 객체
-// HTML 코드를 Javascript 관점에서 보는 것
+// event: on 으로 시작하는 것들
 
-const title = document.getElementById("title");
+const title = document.querySelector("h1");
 
-console.dir(title);
-// <h1> 하나에서 무수히 많은 것들을 가져올 수 있다.
+const superEventHandler = {
+  enter: function handleMouseEnter() {
+    title.style.color = "#3498db";
+    title.innerText = "The mouse is here!";
+  },
 
-title.innerText = "got you!!";
-// JS 에서 어떤 HTML element 이든지 가져올 수 있고, 변경할 수 있다.
+  leave: function handleMouseLeave() {
+    title.style.color = "#9b59b6";
+    title.innerText = "The mouse is gone!";
+  },
+
+  resize: function handleResize() {
+    title.style.color = "#f39c12";
+    title.innerText = "You just resized!";
+  },
+
+  click: function handleContextmenu() {
+    title.style.color = "#1abc9c";
+    title.innerText = "That was a right click!";
+  },
+};
+
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
+window.addEventListener("resize", handleResize);
+window.addEventListener("contextmenu", handleContextmenu);
+// JS 에게 어떤 event 를 listen 하고 싶은지 알려주기
+// 1) 어떤 event 인지 명확히 하기 2) 그 때 어떤 함수 실행시킬지 명시하기
+// 인자의 함수는, 내가 click 했을 때 JS 가 대신 그 함수를 실행시키기 위해
