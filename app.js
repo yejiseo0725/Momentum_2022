b9728def2800994ac0e70f827c1c3e98faed7072
yeji seo <yejiseo0725@gmail.com>
2022-02-12 00:48:03 +0900

    conditional part 1

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
-// event: on 으로 시작하는 것들
+// const body = document.body;
 
-const body = document.body;
+// function handleResize() {
+//   const width = window.innerWidth;
+//   if (width <= 600) {
+//     body.style.backgroundColor = "#0066ff";
+//   } else if (width > 600 && width <= 900) {
+//     body.style.backgroundColor = "#ff4949";
+//   } else {
+//     body.style.backgroundColor = "#ffc82c";
+//   }
+// }
 
-function handleResize() {
-  const width = window.innerWidth;
-  if (width <= 600) {
-    body.style.backgroundColor = "#0066ff";
-  } else if (width > 600 && width <= 900) {
-    body.style.backgroundColor = "#ff4949";
-  } else {
-    body.style.backgroundColor = "#ffc82c";
-  }
-}
+// window.addEventListener("resize", handleResize);
 
-window.addEventListener("resize", handleResize);
+const age = parseInt(prompt("몇 살이니?"));
+console.log(age);
+// 요즘에는 대부분이 HTML, CSS 로 만든 자신만의 창을 사용한다.
+// function: 내부부터 외부로 실행
 
-// JS 에게 어떤 event 를 listen 하고 싶은지 알려주기
-// 1) 어떤 event 인지 명확히 하기 2) 그 때 어떤 함수 실행시킬지 명시하기
-// 인자의 함수는, 내가 click 했을 때 JS 가 대신 그 함수를 실행시키기 위해
+// * Data type 변경하기
+// parseInt("15");
+// Converts a string to an integer
+
+// console.log(age, parseInt(age));
+// 사용자에게 나이를 물어보고, 숫자가 아닌걸 입력 받았을 경우 인지 가능
+// 숫자가 아니라면 NaN (Not a Number) 보여줌
