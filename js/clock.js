89bd208171105c3eb290dd47264d4c7717717cbd
yeji seo <yejiseo0725@gmail.com>
2022-02-16 08:26:54 +0900

    clock

--- js/clock.js
+++ js/clock.js
@@ -6,7 +6,10 @@ const clock = document.querySelector("#clock");
 
 function getClock() {
   const date = new Date();
-  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getClock();
